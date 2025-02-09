85b127183a441cc74276d1463b76c15fa5e05721
Jenis <jeniskapadiya99@gmail.com>
2025-02-09 11:26:27 +0530

    - Refactor assets path.

--- lib/resources/app_assets.dart
+++ lib/resources/app_assets.dart
@@ -1,25 +1,27 @@
 class AppAssets {
-  static const String baseImagePath = "assets/image";
-  static const String basePngPath = "$baseImagePath/png";
-  static const String splashBackground = "$basePngPath/splash_background_img.png";
-  static const String appLogo = "$basePngPath/app_logo.png";
-  static const String bottomBarShadow = "$basePngPath/bottom_bar_shadow.png";
-  static const String walletIcon = "$basePngPath/wallet_icon.png";
-  static const String bottomSheetShadow = "$basePngPath/bottom_sheet_shadow.png";
+  static const String baseImagePath = "assets/image/png";
+  static const String baseIconPath = "assets/icon/png";
+
+  /// Images
+  static const String splashBackground = "$baseImagePath/splash_background_img.png";
+  static const String bottomBarShadow = "$baseImagePath/bottom_bar_shadow.png";
+  static const String bottomSheetShadow = "$baseImagePath/bottom_sheet_shadow.png";
 
   // Icon
-  static const String pcGameIcon = "$basePngPath/pc_game_icon.png";
-  static const String mobileGameIcon = "$basePngPath/mobile_game_icon.png";
-  static const String rupeesIcon = "$basePngPath/rupees_currency_logo.png";
+  static const String pcGameIcon = "$baseIconPath/pc_game_icon.png";
+  static const String appLogo = "$baseIconPath/app_logo.png";
+  static const String walletIcon = "$baseIconPath/wallet_icon.png";
+  static const String mobileGameIcon = "$baseIconPath/mobile_game_icon.png";
+  static const String rupeesIcon = "$baseIconPath/rupees_currency_logo.png";
 
   /// Dashboard icons
-  static const String homeIcon = "$basePngPath/home_icon.png";
-  static const String myMatchesIcon = "$basePngPath/my_matches_icon.png";
-  static const String friendsIcon = "$basePngPath/friends_icon.png";
-  static const String profileIcon = "$basePngPath/profile_icon.png";
+  static const String homeIcon = "$baseIconPath/home_icon.png";
+  static const String myMatchesIcon = "$baseIconPath/my_matches_icon.png";
+  static const String friendsIcon = "$baseIconPath/friends_icon.png";
+  static const String profileIcon = "$baseIconPath/profile_icon.png";
 
   // Social login icon
-  static const String facebookIcon = "$basePngPath/facebook_logo.png";
-  static const String googleIcon = "$basePngPath/google_logo.png";
-  static const String appleLogin = "$basePngPath/apple_lpgo.png";
+  static const String facebookIcon = "$baseIconPath/facebook_logo.png";
+  static const String googleIcon = "$baseIconPath/google_logo.png";
+  static const String appleLogin = "$baseIconPath/apple_lpgo.png";
 }
