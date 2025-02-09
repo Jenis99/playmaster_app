42e8ac08688bb9f6345361f142907f7d90412621
Jenis <jeniskapadiya99@gmail.com>
2025-02-09 12:47:49 +0530

    - Refactor code.

--- lib/module/home/widget/home_widget.dart
+++ lib/module/home/widget/home_widget.dart
@@ -1,6 +1,7 @@
 export 'custom_carousel_slider.dart';
 export 'game_card.dart';
-export 'game_detail_card_tile.dart';
+export 'game_detail_widget/bottom_sheet_view.dart';
+export 'game_detail_widget/game_detail_card_tile.dart';
 export 'home_app_bar.dart';
 export 'top_game_card.dart';
 export 'tournament_title.dart';
